f968c812c2a77e6b7b33cbc8c717dfd5e196a4a0
LightDestory <apb231@gmail.com>
2023-03-19 16:54:29 +0100

    refactor(btetree): Improve search and search speed

--- src/engines/btetree.py
+++ src/engines/btetree.py
@@ -1,33 +1,30 @@
-# VERSION: 1.2
+# VERSION: 1.3
 # AUTHORS: LightDestory (https://github.com/LightDestory)
 
 import re
 import urllib.parse
-from helpers import retrieve_url, download_file
+from helpers import retrieve_url
 from novaprinter import prettyPrinter
 
 
 class btetree(object):
-    url = "http://bt.etree.org/"
+    url = "https://bt.etree.org/"
     name = "bt.etree"
     supported_categories = {'all': '0'}
-    max_pages = 10
 
     class HTMLParser:
 
         def __init__(self, url):
             self.url = url
-            self.pageResSize = 0
+            self.noTorrents = False
 
         def feed(self, html):
-            self.pageResSize = 0
+            self.noTorrents = False
             torrents = self.__findTorrents(html)
-            resultSize = len(torrents)
-            if resultSize == 0:
+            if len(torrents) == 0:
+                self.noTorrents = True
                 return
-            else:
-                self.pageResSize = resultSize
-            for torrent in range(resultSize):
+            for torrent in range(len(torrents)):
                 data = {
                     'link': torrents[torrent][0],
                     'name': torrents[torrent][1],
@@ -35,7 +32,7 @@ class btetree(object):
                     'seeds': torrents[torrent][3],
                     'leech': torrents[torrent][4],
                     'engine_url': self.url,
-                    'desc_link': urllib.parse.unquote(torrents[torrent][0])
+                    'desc_link': torrents[torrent][5],
                 }
                 prettyPrinter(data)
 
@@ -46,32 +43,27 @@ class btetree(object):
             for tr in trs:
                 # Extract from the A node all the needed information
                 url_titles = re.search(
-                    r'<tr.+?details_link\" href=\"(.+?)\".+?<b>(.+?)</b>.+?([0-9\,\.]+ (TB|GB|MB|KB)).+?seeders\">([0-9,]+).+?leechers\">([0-9,]+)',
+                    r'<tr.+?details_link.+?href=\"(.+?)\".+?<b>(.+?)</b>.+?href=\"(.+?)\".+?([0-9\,\.]+ (TB|GB|MB|KB)).+?seeders\">([0-9,]+).+?leechers\">([0-9,]+)',
                     tr)
                 if url_titles:
                     torrents.append([
-                        urllib.parse.quote('{0}{1}'.format(self.url, url_titles.group(1))),
+                        '{0}{1}'.format(self.url, urllib.parse.quote(url_titles.group(3))),
                         url_titles.group(2),
-                        url_titles.group(3).replace(",", ""),
-                        url_titles.group(5).replace(",", ""),
-                        url_titles.group(6).replace(",", "")
+                        url_titles.group(4).replace(",", ""),
+                        url_titles.group(6).replace(",", ""),
+                        url_titles.group(7).replace(",", ""),
+                        '{0}{1}'.format(self.url, url_titles.group(1))
                     ])
             return torrents
-
-    def download_torrent(self, info):
-        torrent_page = retrieve_url(urllib.parse.unquote(info))
-        file_link = re.search(r'.+?href=\"(.+?\.torrent)\"', torrent_page)
-        if file_link and file_link.groups():
-            print(download_file(self.url + file_link.groups()[0]))
-        else:
-            raise Exception('Error, please fill a bug report!')
 
     def search(self, what, cat='all'):
         what = what.replace('%20', '+')
         parser = self.HTMLParser(self.url)
-        for currPage in range(0, self.max_pages):
-            url = '{0}index.php?searchzzzz={1}&cat=0&page={2}'.format(self.url, what, 50*currPage)
+        counter = 0
+        while True:
+            url = '{0}index.php?searchzzzz={1}&cat=0&page={2}'.format(self.url, what, 50 * counter)
             html = retrieve_url(url).replace("	", "").replace("\n", "").replace("\r", "")
             parser.feed(html)
-            if parser.pageResSize <= 0:
+            if parser.noTorrents:
                 break
+            counter += 1
