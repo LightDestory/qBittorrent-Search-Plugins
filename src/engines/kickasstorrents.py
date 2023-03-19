ae4eb10eebba8d86af5efe3d72bd028e3603a509
LightDestory <apb231@gmail.com>
2023-03-19 20:33:39 +0100

    refactor(kickasstorrents): Improve search and add sleeps

--- src/engines/kickasstorrents.py
+++ src/engines/kickasstorrents.py
@@ -1,8 +1,8 @@
-# VERSION: 1.0
+# VERSION: 1.1
 # AUTHORS: LightDestory (https://github.com/LightDestory)
 
 import re
-import urllib.parse
+from time import sleep
 from helpers import retrieve_url
 from novaprinter import prettyPrinter
 
@@ -12,36 +12,20 @@ class kickasstorrents(object):
     name = 'Kickasstorrents'
 
     supported_categories = {'all': '', 'movies': 'movies', 'tv': 'tv', 'music': 'music', 'games': 'games', 'anime': 'anime', 'software': 'apps'}
-    max_pages = 10
-
     class HTMLParser:
 
         def __init__(self, url):
             self.url = url
-            self.pageResSize = 0
+            self.noTorrents = False
 
         def feed(self, html):
-            self.pageResSize = 0
+            self.noTorrents = False
             torrents = self.__findTorrents(html)
-            resultSize = len(torrents)
-            if resultSize == 0:
+            if len(torrents) == 0:
+                self.noTorrents = True
                 return
-            else:
-                self.pageResSize = resultSize
-            for torrent in range(resultSize):
-                data = {
-                    'link': torrents[torrent][0],
-                    'name': torrents[torrent][1],
-                    'size': torrents[torrent][2],
-                    'seeds': torrents[torrent][3],
-                    'leech': torrents[torrent][4],
-                    'engine_url': self.url,
-                    'desc_link': urllib.parse.unquote(torrents[torrent][0])
-                }
-                prettyPrinter(data)
 
         def __findTorrents(self, html):
-            torrents = []
             # Find all TR nodes with class odd or even
             trs = re.findall(r'<tr class=\"(?:odd|even)\">.*?</tr>', html)
             for tr in trs:
@@ -49,31 +33,39 @@ class kickasstorrents(object):
                     r'.+?torrentname.+?href=\"(.+?)\".+?cellMainLink\">(.+?)<.+?nobr.+?>([0-9\.\,]+ (TB|GB|MB|KB)).+?green.+?>([0-9,]+).+?red.+?>([0-9,]+)',
                     tr)
                 if url_titles:
-                    torrents.append([
-                        urllib.parse.quote('{0}{1}'.format(self.url, url_titles.group(1))),
-                        url_titles.group(2),
-                        url_titles.group(3).replace(",", ""),
-                        url_titles.group(5).replace(",", ""),
-                        url_titles.group(6).replace(",", "")
-                    ])
-            return torrents
+                    detail_link = '{0}{1}'.format(self.url, url_titles.group(1))
+                    download_link = self.__retrieve_download_link(detail_link)
+                    data = {
+                        'link': download_link,
+                        'name': url_titles.group(2),
+                        'size': url_titles.group(3).replace(",", ""),
+                        'seeds': url_titles.group(5).replace(",", ""),
+                        'leech': url_titles.group(6).replace(",", ""),
+                        'engine_url': self.url,
+                        'desc_link': detail_link,
+                    }
+                    prettyPrinter(data)
+                    sleep(1)
+            return trs
 
-    def download_torrent(self, info):
-        torrent_page = retrieve_url(urllib.parse.unquote(info))
-        magnet_match = re.search(r'\"(magnet:.*?)\"', torrent_page)
-        if magnet_match and magnet_match.groups():
-            print('{0} {1}'.format(magnet_match.groups()[0], info))
-        else:
-            raise Exception('Error, please fill a bug report!')
+        def __retrieve_download_link(self, detail_link):
+            torrent_page = retrieve_url(detail_link)
+            magnet_match = re.search(r'\"(magnet:.*?)\"', torrent_page)
+            if magnet_match and magnet_match.groups():
+                return str(magnet_match.groups()[0])
+            else:
+                return "NotFound"
 
     def search(self, what, cat='all'):
         parser = self.HTMLParser(self.url)
         category = "" if cat == "all" else 'category/{0}/'.format(self.supported_categories[cat])
-        for currPage in range(0, self.max_pages):
-            url = '{0}search/{1}/{2}{3}/'.format(self.url, what, category, currPage)
+        counter: int = 0
+        while True:
+            url = '{0}search/{1}/{2}{3}/'.format(self.url, what, category, counter)
             # Some replacements to format the html source
             html = retrieve_url(url).replace("	", "").replace("\n", "").replace("\r", "")\
                 .replace("<strong class=\"red\">", "").replace("</strong>", "")
             parser.feed(html)
-            if parser.pageResSize <= 0:
+            if parser.noTorrents:
                 break
+            counter += 1
