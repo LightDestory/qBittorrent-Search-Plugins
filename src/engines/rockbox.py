761a19a6868857b7fd169f31c5596bb5daa4b2a5
LightDestory <apb231@gmail.com>
2023-03-19 22:25:36 +0100

    refactor(rockbox): Improve search

--- src/engines/rockbox.py
+++ src/engines/rockbox.py
@@ -1,8 +1,7 @@
-# VERSION: 1.0
+# VERSION: 1.1
 # AUTHORS: LightDestory (https://github.com/LightDestory)
 
 import re
-import urllib.parse
 from helpers import retrieve_url, download_file
 from novaprinter import prettyPrinter
 
@@ -16,24 +15,20 @@ class rockbox(object):
         qBittorrent does not provide enough categories to implement a good filtering.
     """
     supported_categories = {'all': '0'}
-    # RockBox's search divided into pages, so we are going to set a limit on how many pages to read
-    max_pages = 10
 
     class HTMLParser:
 
         def __init__(self, url):
             self.url = url
-            self.pageResSize = 0
+            self.noTorrents = False
 
         def feed(self, html):
-            self.pageResSize = 0
+            self.noTorrents = False
             torrents = self.__findTorrents(html)
-            resultSize = len(torrents)
-            if resultSize == 0:
+            if len(torrents) == 0:
+                self.noTorrents = True
                 return
-            else:
-                self.pageResSize = resultSize
-            for torrent in range(resultSize):
+            for torrent in range(len(torrents)):
                 data = {
                     'link': torrents[torrent][0],
                     'name': torrents[torrent][1],
@@ -41,7 +36,7 @@ class rockbox(object):
                     'seeds': torrents[torrent][3],
                     'leech': torrents[torrent][4],
                     'engine_url': self.url,
-                    'desc_link': urllib.parse.unquote(torrents[torrent][0])
+                    'desc_link': torrents[torrent][5]
                 }
                 prettyPrinter(data)
 
@@ -51,33 +46,28 @@ class rockbox(object):
             for tr in trs:
                 # Extract from the A node all the needed information
                 url_titles = re.search(
-                    r'.+?HREF=\"(.+?)\".+?>(.+?)</A>.+?([0-9\,\.]+ (TB|GB|MB|KB)).+?peers details\">([0-9,]+).+?peers details\">([0-9,]+).+?',
+                    r'.+?HREF=\"(.+?)\".+?details\: ?(.+?)\">.+?rating.+?HREF=(.+?)>.+?([0-9\,\.]+ (TB|GB|MB|KB)).+?peers details\">([0-9,]+).+?peers details\">([0-9,]+).+?',
                     tr)
                 if url_titles:
                     torrents.append([
-                        urllib.parse.quote('{0}{1}'.format(self.url, url_titles.group(1))),
+                        '{0}{1}'.format(self.url, url_titles.group(3)),
                         url_titles.group(2),
-                        url_titles.group(3).replace(",", ""),
-                        url_titles.group(5).replace(",", ""),
-                        url_titles.group(6).replace(",", "")
+                        url_titles.group(4).replace(",", ""),
+                        url_titles.group(6).replace(",", ""),
+                        url_titles.group(7).replace(",", ""),
+                        '{0}{1}'.format(self.url, url_titles.group(1))
                     ])
             return torrents
-
-    def download_torrent(self, info):
-        torrent_page = retrieve_url(urllib.parse.unquote(info))
-        file_link = re.search(r'.+?HREF=(download\.php\?.+?)>', torrent_page)
-        if file_link and file_link.groups():
-            print(download_file(self.url + file_link.groups()[0]))
-        else:
-            raise Exception('Error, please fill a bug report!')
 
     def search(self, what, cat='all'):
         what = what.replace("%20", "+")
         parser = self.HTMLParser(self.url)
-        for currPage in range(0, self.max_pages):
-            url = '{0}torrents.php?active=0&search={1}&options=0&order=data&page={2}'.format(self.url, what, currPage)
+        counter: int = 0
+        while True:
+            url = '{0}torrents.php?active=0&search={1}&options=0&order=data&page={2}'.format(self.url, what, counter)
             # Some replacements to format the html source
             html = retrieve_url(url).replace("	", "").replace("\n", "").replace("\r", "")
             parser.feed(html)
-            if parser.pageResSize <= 0:
+            if parser.noTorrents:
                 break
+            counter += 1
